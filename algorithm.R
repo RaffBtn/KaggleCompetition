117269ae9854b3ff9024ed513f6d29252ac28a69
RaffBtn <raffaele.bertino@hotmail.com>
2018-04-13 14:46:32 +0200

    Multiple Regression /w every parameter

--- algorithm.R
+++ algorithm.R
@@ -2,5 +2,7 @@ train <- read.csv("train.csv", sep=",")
 test <- read.csv("test.csv", sep=",")
 sample_sol <- read.csv("sample_sol.csv", sep=",")
 
-model <- lm(shares ~ n_tokens_title + n_tokens_content + n_unique_tokens + n_non_stop_words + n_non_stop_unique_tokens + num_hrefs + num_self_hrefs + num_imgs + num_videos + average_token_length + num_keywords, data=data)
+f1 <- as.formula(paste("shares ~", paste(names(train)[c(1:44)], 
+                                    collapse="+")))
+model <- lm(f1, data=train)
 sharestest <- predict(model, test)
