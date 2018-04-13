9c1d319beff24460ac1eb283993de1d38fb15746
RaffBtn <raffaele.bertino@hotmail.com>
2018-04-13 14:37:47 +0200

    multiple reg

--- algorithm.R
+++ algorithm.R
@@ -1,2 +1,6 @@
-data <- read.csv("train.csv", sep=",")
+train <- read.csv("train.csv", sep=",")
+test <- read.csv("test.csv", sep=",")
+sample_sol <- read.csv("sample_sol.csv", sep=",")
 
+model <- lm(shares ~ n_tokens_title + n_tokens_content + n_unique_tokens + n_non_stop_words + n_non_stop_unique_tokens + num_hrefs + num_self_hrefs + num_imgs + num_videos + average_token_length + num_keywords, data=data)
+sharestest <- predict(model, test)
